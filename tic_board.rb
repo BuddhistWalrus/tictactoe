16c35e3b547da04640b1e9ee1d4fa686f93860b8
BuddhistWalrus <Gabe>
2014-06-24 10:07:44 +0200

    working win method maybe

--- tic_board.rb
+++ tic_board.rb
@@ -5,27 +5,40 @@ class TicBoard
 		@board = [{0 => 0}, {1 => 0}, {2 => 0}, {3 => 0}, {4 => 0}, {5 => 0}, {6 => 0}, {7 => 0}, {8 => 0}]
 		@x = 1
 		@o = 2
-		@solutions = [[0,1,2][0,3,6][0,4,8][1,4,7][2,5,8][2,4,6][3,4,5][6,7,8]]
+		@solutions = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
 		@x_location = []
 		@o_location = []
 	end
 
 	#add x to someplace on the board and record the location
 	def add_x(index)
-		@board.each do |key, value|
-			if key == index
-				value = @x
-				@x_location << key
+		puts "Working"
+		@board.each do |place|
+			place.each do |key, value|
+				puts "#{key}: #{index}"
+				if key == index
+					value = @x
+					@x_location << index
+				end
 			end
 		end
 	end
 
+	def x_location
+		@x_location
+	end
+
+	def o_location
+		@o_location
+	end
 	#add o to someplace on the board and record the location
 	def add_o(index)
+		puts "Working"
 		@board.each do |key, value|
+			puts "#{key}: #{index}"
 			if key == index
 				value = @o
-				@o_location << key
+				@o_location << index
 			end
 		end
 	end
@@ -40,11 +53,17 @@ class TicBoard
 			elsif ((solution - @o_location) == [])
 				return "O Wins!"
 
-			else
-				return "Draw!"
 
 			end
 		end
+		return "Draw!"
 	end
+end
 
+foo = TicBoard.new
 
+foo.add_x(0)
+foo.add_x(1)
+foo.add_x(2)
+puts "#{foo.x_location}"
+puts foo.determine_winner
