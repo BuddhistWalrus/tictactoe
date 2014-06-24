29121e75aebc756a39e57476a83a3cc8668b781b
BuddhistWalrus <Gabe>
2014-06-24 23:15:44 +0200

    comments on ticboard

--- tic_board.rb
+++ tic_board.rb
@@ -5,17 +5,21 @@ class TicBoard
 		@board = [{0 => 0}, {1 => 0}, {2 => 0}, {3 => 0}, {4 => 0}, {5 => 0}, {6 => 0}, {7 => 0}, {8 => 0}]
 		@x = 1
 		@o = 2
+
+		#all possible solutions 
 		@solutions = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
+
+		#arrays to hold the locations of placed x's and o's
 		@x_location = []
 		@o_location = []
 	end
 
 	#add x to someplace on the board and record the location
 	def add_x(index)
-		puts "Working"
+	
 		@board.each do |place|
 			place.each do |key, value|
-				puts "#{key}: #{index}"
+				
 				if key == index
 					value = @x
 					@x_location << index
@@ -33,9 +37,9 @@ class TicBoard
 	end
 	#add o to someplace on the board and record the location
 	def add_o(index)
-		puts "Working"
+		
 		@board.each do |key, value|
-			puts "#{key}: #{index}"
+			
 			if key == index
 				value = @o
 				@o_location << index
