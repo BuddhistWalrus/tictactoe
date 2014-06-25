6775afae5dad1a16502b5932c79396640af06eab
BuddhistWalrus <Gabe>
2014-06-25 08:29:44 +0200

    fixed error checks and other bugs in board logic

--- tic_board.rb
+++ tic_board.rb
@@ -2,12 +2,12 @@ class TicBoard
 
 	#init with full board, solutions, and location arrays
 	def initialize
-		@board = [{0 => 0}, {1 => 0}, {2 => 0}, {3 => 0}, {4 => 0}, {5 => 0}, {6 => 0}, {7 => 0}, {8 => 0}]
+		@board = [{1 => 0}, {2 => 0}, {3 => 0}, {4 => 0}, {5 => 0}, {6 => 0}, {7 => 0}, {8 => 0}, {9 => 0}]
 		@x = 1
 		@o = 2
 
 		#all possible solutions 
-		@solutions = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
+		@solutions = [[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9]]
 
 		#arrays to hold the locations of placed x's and o's
 		@x_location = []
@@ -15,17 +15,26 @@ class TicBoard
 	end
 
 	#add x to someplace on the board and record the location
-	def add_x(index)
-	
-		@board.each do |place|
-			place.each do |key, value|
-				
-				if key == index
-					value = @x
-					@x_location << index
+	def add_x(index, spaces)
+		if index > 0 && index <=9
+			@board.each do |place|
+				place.each do |key, value|
+					
+					if key == index
+						place[key] = @x
+						if !(@o_location.include?(index) || @x_location.include?(index))
+							@x_location << index
+							spaces += 1
+						else
+							puts "Invalid move!"
+						end
+					end
 				end
 			end
+		else
+			puts "Invalid move!"
 		end
+		spaces
 	end
 
 	def x_location
@@ -37,35 +46,45 @@ class TicBoard
 	end
 
 	#add o to someplace on the board and record the location
-	def add_o(index)
-		
-		@board.each do |place|
-			place.each do |key, value|
-			
-				if key == index
-					value = @o
-					@o_location << index
+	def add_o(index, spaces)
+		if index > 0 && index <=9
+			@board.each do |place|
+				place.each do |key, value|
+				
+					if key == index
+						place[key] = @o
+						if !(@x_location.include?(index) || @o_location.include?(index))
+							@o_location << index
+							spaces += 1
+						else 
+							puts "Invalid move!"
+						end
+					end
 				end
 			end
+		else
+			puts "Invalid move!"
 		end
+		spaces
 	end
 
 	#loop through every possible solution and use array subtraction to determine the winner
-	def determine_winner(turns)
+	def determine_winner(spaces)
+
 		@solutions.each do |solution|
 
+			
 			if ((solution - @x_location) == [])
 				return "X Wins!"
 			
 			elsif ((solution - @o_location) == [])
 				return "O Wins!"
 
-			elsif turns == 9
+			elsif spaces == 9
 				return "Draw!"
-
-			else
-				return nil
+				
 			end
 		end
+		return nil
 	end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,35 +1,36 @@
-require_relative 'tic_board.rb'
+require_relative 'tic_board'
 
 puts "Welcome to tic tac toe!"
 
 turn = "T"
-num_of_turns = 0
+filled_spaces = 0
 
 game_board = TicBoard.new
 
 while true
-	num_of_turns += 1
+	
 
 	#say which player's turn it is
 	if turn == "T"
 		puts "It's player 1's turn! Please choose a location for x:"
-		game_board.add_x(gets.strip.to_i)
+		
+		filled_spaces = game_board.add_x(gets.strip.to_i, filled_spaces)
 	else
 		puts "It's player 2's turn! Please choose a location for o:"
-		game_board.add_o(gets.strip.to_i)
+		filled_spaces = game_board.add_o(gets.strip.to_i, filled_spaces)
 	end
 
 	#check to see if a winner can be determined
-	if num_of_turns >= 5
+	if filled_spaces >= 5
 
-		if game_board.determine_winner(num_of_turns)
+		if game_board.determine_winner(filled_spaces)
 	
-			puts game_board.determine_winner(num_of_turns)
+			puts game_board.determine_winner(filled_spaces)
 			break
 		end
 	end
 
-	break if num_of_turns == 9
+	#break if filled_spaces == 9
 
 	#switch player's turn using swapcase
 	turn = turn.swapcase
